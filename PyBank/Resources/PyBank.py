2770fd85b7a3e5417cc4de4f9cf2bbba7c65a55f
E-Weber <ewebersms@gmail.com>
2021-02-08 14:45:13 -0600

    Created variables

---
+++ PyBank/Resources/PyBank.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+TotalMonths = 0
+TotalProfitLoss = 0.00
+AvgProfitLoss = 0.00
+GreatestProfit = []
+GreatestLoss = []
+
+
+PyBankcsv = "../Resources/Budget_data.csv"
+with open(PyBankcsv, 'a') as csvfile:
+    # print(csvfile)
+    text.write("test")
+    #lines = csvfile.read()
+    # print(lines)
