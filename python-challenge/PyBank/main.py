6d176de1bf28587c5cc9c33ae982e1f526b1edb7
E-Weber <ewebersms@gmail.com>
2021-02-08 22:22:57 -0600

    progress

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -5,9 +5,10 @@ AverageProfitLoss = 0.00
 GreatestIncrease = {"date": "", "amount": 0}
 GreatestDecrease = {"date": "", "amount": 0}
 LastChange = 0
-RevenueList = []
-Revenueavg = 0
+
+
 month = []
+
 
 file_path = "./Resources/budget_data.csv"
 out_file = "./Analysis/output.txt"
@@ -18,9 +19,14 @@ with open(file_path) as csvfile:
 
     # Read the header row first(skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+
+    #print(f"CSV Header: {csv_header}")
+
     Date = csv_header.index('Date')
     ProfitLoss = csv_header.index('Profit/Losses')
+    ProfitLossList = []
+    RevenueList = []
+    Revenueavg = 0
     # Read each row of data after the header
     for row in csvreader:
         # The total number of months included in the data set
@@ -32,10 +38,13 @@ with open(file_path) as csvfile:
         TotalProfitLoss = TotalProfitLoss + int(row[ProfitLoss])
 
     # Calculate changes in profit/loss over entire period, then find average
-        AverageProfitLoss = float(row[1]) - LastChange
-        LastChange = float(row[1])
+        AverageProfitLoss = float(row[ProfitLoss]) - LastChange
+        LastChange = float(row[ProfitLoss])
+
+    for rows in csvreader:
+        ProfitLossList.append(AverageProfitLoss)
         RevenueList = RevenueList + [LastChange]
-        month = [month] + (row["date"])
+        month = month + (row[Date])
 
     # The greatest increase in losses (date and amount) over the entire period
         if (profit > GreatestIncrease["amount"]):
@@ -45,19 +54,24 @@ with open(file_path) as csvfile:
         if (profit < GreatestDecrease["amount"]):
             GreatestDecrease["date"] = date
             GreatestDecrease["amount"] = profit
-    Revenueavg = sum(RevenueList) / len(RevenueList)
+
+    #Revenueavg = sum(RevenueList) / len(RevenueList)
 
 
 # print results
-print("Finacial Analysis")
-print("-------------")
+print(f"Finacial Analysis")
+print(f"-------------")
 print(f"Total Months: {TotalMonths}")
 print(
-    f"Greatest Increase In Profits: {GreatestIncrease['date']} (${GreatestIncrease['amount']}")
+    f"Greatest Increase In Profits: {GreatestIncrease['date']} ${GreatestIncrease['amount']}")
 print(
-    f"Greatest Decrease In Profits: {GreatestDecrease['date']} (${GreatestDecrease['amount']}")
+    f"Greatest Decrease In Profits: {GreatestDecrease['date']} ${GreatestDecrease['amount']}")
 print(
     f"Total: {TotalProfitLoss}")
+print(
+    f"{ProfitLossList}")
+print(
+    f"Average Change: ${round(mean(ProfitLossList),2):,}")
 
 # write to a file
 with open(out_file, 'w') as outputFile:
