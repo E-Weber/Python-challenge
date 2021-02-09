d83b71e97718f2450b2ae4295d58f71e1d818d1e
E-Weber <ewebersms@gmail.com>
2021-02-09 16:21:46 -0600

    stuck - reset

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -8,30 +8,29 @@ TotalVotes = 0
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
     csv_header = next(csvreader)
-    print(f"CSV Header{csv_header}")
 
     voterID = csv_header.index('Voter ID')
     candidate = csv_header.index('Candidate')
     county = csv_header.index('County')
-    candidateList = []
+    candList = []
 # Read Each row
     for row in csvreader:
         # The total number of votes cast
         TotalVotes = TotalVotes + 1
 
 # A complete list of candidates who received votes
-        candidateList.append(str(row[candidate]))
-
-unique_candidateList = list(set(candidateList))
+        candList.append(str(row[candidate]))
+        unique_candList = list(set(candList))
 # The percentage of votes each candidate won
-
 # The total number of votes each candidate won
+
+# while row[candidate] == [unique_candidateList]:
 
 # The winner of the election based on popular vote.
 
 # RESULTS
 print(f"Total Votes: {TotalVotes}")
-print(f"Candidates: {unique_candidateList}")
+print(f"candidates: {unique_candList}")
 
 # -------------------------
 # Total Votes: 3521001
