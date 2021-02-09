a2ee23ac79073d7df898e4efbe19c72a9c132fc9
E-Weber <ewebersms@gmail.com>
2021-02-09 10:30:22 -0600

    total votes

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -1,23 +1,41 @@
 import os
 import csv
 
-PyPollcsv = ('../Resources, election_data.csv')
+csvpath = "./Resources/election_data.csv"
 
-# The total number of months included in the dataset
+TotalVotes = 0
 
-# The net total amount of "Profit/Losses" over the entire period
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile)
+    csv_header = next(csvreader)
+    print(f"CSV Header{csv_header}")
 
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+    voterID = csv_header.index('Voter ID')
+    candidate = csv_header.index('Candidate')
+    county = csv_header.index('County')
 
-# The greatest increase in profits(date and amount) over the entire period
+# Read Each row
+    for row in csvreader:
+        # The total number of votes cast
+        TotalVotes = TotalVotes + 1
 
-# The greatest decrease in losses(date and amount) over the entire period
+# A complete list of candidates who received votes
 
+# The percentage of votes each candidate won
 
-#  Results Below
-#  ----------------------------
-#  Total Months: 86
-#  Total: $38382578
-#  Average  Change: $-2315.12
-#  Greatest Increase in Profits: Feb-2012 ($1926159)
-#  Greatest Decrease in Profits: Sep-2013 ($-2196167)
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
+# RESULTS
+print("Total Votes: {TotalVotes}")
+# -------------------------
+# Total Votes: 3521001
+# -------------------------
+#Khan: 63.000% (2218231)
+#Correy: 20.000% (704200)
+#Li: 14.000% (492940)
+# O'Tooley: 3.000% (105630)
+# -------------------------
+#Winner: Khan
+# -------------------------
