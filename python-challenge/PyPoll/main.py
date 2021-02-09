9818b32eb7d1064421495354ef9c1833e98333ff
E-Weber <ewebersms@gmail.com>
2021-02-09 16:51:56 -0600

    votes percent and print

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -21,7 +21,7 @@ with open(csvpath) as csvfile:
 # A complete list of candidates who received votes
         candList.append(str(row[candidate]))
         unique_candList = list(set(candList))
-# The percentage of votes each candidate won
+
 # The total number of votes each candidate won
     votes1 = unique_candList[0]
     votes1_count = candList.count(votes1)
@@ -31,13 +31,20 @@ with open(csvpath) as csvfile:
     votes3_count = candList.count(votes3)
     votes4 = unique_candList[3]
     votes4_count = candList.count(votes4)
-# while row[candidate] == [unique_candidateList]:
+# The percentage of votes each candidate won
+    votes1_p = round((votes1_count / TotalVotes) * 100)
+    votes2_p = round((votes2_count / TotalVotes) * 100)
+    votes3_p = round((votes3_count / TotalVotes) * 100)
+    votes4_p = round((votes4_count / TotalVotes) * 100)
 
 # The winner of the election based on popular vote.
 
 # RESULTS
 print(f"Total Votes: {TotalVotes}")
-print(f"candidates: {unique_candList}")
+print(f"{votes1}: {votes1_p}.000%({votes1_count})")
+print(f"{votes2}: {votes2_p}.000%({votes2_count})")
+print(f"{votes3}: {votes3_p}.000%({votes3_count})")
+print(f"{votes4}: {votes4_p}.000%({votes4_count})")
 
 # -------------------------
 # Total Votes: 3521001
